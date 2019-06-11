c0bcc95cc882803ec191d845ef6b86f40acd00af
Craig Donavin <crdonavin@gmail.com>
2019-06-11 01:01:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,39 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    profiles = []
+    html = open(index_url)
+    text = Nokogiri::HTML(html)
+    text.css('.roster-cards-container').each do |card|
+      card.css('.student-card a').each do |profile|
+        profile_url = "#{profile.attr('href')}"
+        name = profile.css(".student-name").text
+        location = profile.css(".student-location").text
+        profiles << {name: name, location: location, profile_url: profile_url}
+      end
+    end
+    # binding.pry
+    profiles
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
-
+    hash = {}
+    text= Nokogiri::HTML(open(profile_url))
+      pages = text.css(".social-icon-container").children.css("a").map {|icon| icon.attribute("href")}
+        pages.each do |icon|
+        if icon.include?("twitter")
+          hash[:twitter]= icon
+        elsif icon.include?("linkedin")
+          hash[:linkedin]= icon
+        elsif icon.include?("github")
+          hash[:github]= icon
+        else
+          hash[:blog]= icon
+        end
+        binding.pry
+          profile_quote = ""
+        bio = ""
+      end
+      hash
+    end
 end
-
